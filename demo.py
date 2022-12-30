e4143b8786e2ee960c03311136b661f3d6c35f31
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2022-12-30 15:59:48 -0600

    reworked demo.py, added images.py

--- demo.py
+++ demo.py
@@ -1,79 +1,6 @@
 from bruhscreen import WinScreen
 from bruhrenderer import *
-
-bruh2_0 = [
-    r"loading BRUH SHELL 2.0 loading BRUH SHELL 2.0 loading BRUH ",
-    r"loading BRUH SHELL 2.0 loading BRUH SHELL 2.0 loading BRUH ",
-    r"loading BRUH SHELL 2.0 loading BRUH SHELL 2.0 loading BRUH ",
-    r"loading BRUH         _             _           oading BRUH ",
-    r"loading BRUH       /\ \          / /\          oading BRUH ",
-    r"loading BRUH      /  \ \        / /  \         oading BRUH ",
-    r"loading BRUH     / /\ \ \      / / /\ \        oading BRUH ",
-    r"loading BRUH     \/_/\ \ \    / / /\ \ \       oading BRUH ",
-    r"loading BRUH         / / /   /_/ /  \ \ \      oading BRUH ",
-    r"loading BRUH        / / /    \ \ \   \ \ \     oading BRUH ",
-    r"loading BRUH       / / /  _   \ \ \   \ \ \    oading BRUH ",
-    r"loading BRUH      / / /_/\_\ _ \ \ \___\ \ \   oading BRUH ",
-    r"loading BRUH     / /_____/ //\_\\ \/____\ \ \  oading BRUH ",
-    r"loading BRUH     \________/ \/_/ \_________\/  oading BRUH ",
-    r"loading BRUH                                   oading BRUH ",
-    r"loading BRUH SHELL 2.0 loading BRUH SHELL 2.0 loading BRUH ",
-    r"loading BRUH SHELL 2.0 loading BRUH SHELL 2.0 loading BRUH ",
-    r"loading BRUH SHELL 2.0 loading BRUH SHELL 2.0 loading BRUH ",
-]
-bruh2_0_2 = [
-    r"    _             _          ",
-    r"  /\ \          / /\         ",
-    r" /  \ \        / /  \        ",
-    r"/ /\ \ \      / / /\ \       ",
-    r"\/_/\ \ \    / / /\ \ \      ",
-    r"    / / /   /_/ /  \ \ \     ",
-    r"   / / /    \ \ \   \ \ \    ",
-    r"  / / /  _   \ \ \   \ \ \   ",
-    r" / / /_/\_\ _ \ \ \___\ \ \  ",
-    r"/ /_____/ //\_\\ \/____\ \ \ ",
-    r"\________/ \/_/ \_________\/ ",
-]
-computer = [
-    f"                       .,,uod8B8bou,,.                             ",
-    f"              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:.                    ",
-    f"         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||                   ",
-    f"         !...:!TVBBBRPFT||||||||||!!^^\"\"'    |||                   ",
-    f"         !.......:!?|||||!!^^\"\"'             |||                   ",
-    f"         !.........|||   ___ ___ _   _ _  _  |||                   ",
-    f"         !.........|||  | _ ) _ \ | | | || | |||                   ",
-    f"         !.........|||  | _ \   / |_| | __ | |||                   ",
-    f"         !.........|||  |___/_|_\\\___/|_||_| |||                   ",
-    f"         !.........|||   ---   --            |||                   ",
-    f"         !.........|||  |_  ) /  \           |||                   ",
-    f"         `.........|||   / / | () |        , |||                   ",
-    f"          .;.......|||  /___(_)__/     _.-!!||||                   ",
-    f"   .,uodWBBBBb.....|||        _.-!!|||||||||!:'                    ",
-    f"!YBBBBBBBBBBBBBBb..!|||:..-!!|||||||!iof68BBBBBb....               ",
-    f"!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   `.             ",
-    f"!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     `.           ",
-    f"!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^\"`;:::       `.         ",
-    f"!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.   ",
-    f"`..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo. ",
-    f"  `..........:::::::::::::::::::::::;iof688888888888b.     `YBBBP^'",
-    f"    `........::::::::::::::::;iof688888888888888888888b.     `     ",
-    f"      `......:::::::::;iof688888888888888888888888888888b.         ",
-    f"        `....:::;iof688888888888888888888888888888888899fT!        ",
-    f"          `..::!8888888888888888888888888888888899fT|!^\"'          ",
-    f"            `' !!988888888888888888888888899fT|!^\"'                ",
-    f"                `!!8888888888888888899fT|!^\"'                      ",
-    f"                  `!988888888899fT|!^\"'                            ",
-    f"                    `!9899fT|!^\"'                                  ",
-    f"                      `!^\"'                                        "
-]
-hey = [
-  f"    __  __          ",
-  f"   / / / /__  __  __",
-  f"  / /_/ / _ \/ / / /",
-  f" / __  /  __/ /_/ / ",
-  f"/_/ /_/\___/\__, /  ",
-  f"           /____/   "
-]
+import images
 
 
 def plasma_render(screen, frames, time, effect, background, transparent):
@@ -95,7 +22,7 @@ def noise_render(screen, frames, time, effect, background, transparent):
     renderer = EffectRenderer(screen, frames, time, effect, background, transparent)
 
     # REDUCE INTENSITY FOR FASTER RENDER
-    renderer.effect.update_intensity(1)
+    renderer.effect.update_intensity(10)
 
     # RUN
     renderer.run(end_message=False)
@@ -149,10 +76,9 @@ def offset_render(screen, frames, time, effect, background, transparent):
     input()
 
 
+def main():
 
-
-if __name__ == "__main__":
-    FRAMES = 1000
+    FRAMES = 500
 
     # TESTING NOISE
     WinScreen.show(noise_render, args=(FRAMES, 0, "noise", " ", None))
@@ -168,3 +94,7 @@ if __name__ == "__main__":
 
     # TESTING OFFSET
     WinScreen.show(offset_render, args=(FRAMES, 0, "offset", "..--..__", None))
+
+
+if __name__ == "__main__":
+    main()
