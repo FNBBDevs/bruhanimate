4c703de80ee72081dc0870b1e38b32225f7d381c
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2023-01-03 18:39:52 -0600

    screen header

--- bruhanimate/bruhscreen.py
+++ bruhanimate/bruhscreen.py
@@ -1,7 +1,18 @@
 """
-Much of bruhscreen implementation is based on the Windows based implementation of 
-setting of terminal screen from AsciiMatics found here at <https://github.com/peterbrittain/asciimatics>
-and more specifically at <https://github.com/peterbrittain/asciimatics/blob/master/asciimatics/screen.py>
+Copyright 2023 Ethan Christensen
+Copied, Guided, and Adapted from Asciimatics <https://github.com/peterbrittain/asciimatics/blob/master/asciimatics/screen.py>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 from __future__ import division
