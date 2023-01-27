898ac4f191d81f50d9cfce8f25794635d7a8863c
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2023-01-26 19:31:41 -0600

    moved demos

--- bruhanimate/__init__.py
+++ bruhanimate/__init__.py
@@ -1,14 +1,16 @@
-import sys
 from bruhanimate.bruhscreen import Screen
-from bruhanimate.bruheffects    import BaseEffect, StaticEffect, OffsetEffect, NoiseEffect, StarEffect, PlasmaEffect, GameOfLifeEffect, RainEffect
+from bruhanimate.bruheffects    import BaseEffect, StaticEffect, OffsetEffect, NoiseEffect, StarEffect, PlasmaEffect, GameOfLifeEffect, RainEffect, MatrixEffect, _LINE, DrawLines
 from bruhanimate.bruhffer       import Buffer
-from bruhanimate.bruhrenderer   import BaseRenderer, EffectRenderer, CenterRenderer, PanRenderer
+from bruhanimate.bruhrenderer   import BaseRenderer, EffectRenderer, CenterRenderer, PanRenderer, FocusRenderer
 from bruhanimate import images
-import demo
+from bruhanimate import demo
+from bruhanimate import line_demo
+
 
 __all__ = [
     "Screen",
     "demo"
+    "line_demo"
     "BaseEffect",
     "StaticEffect",
     "OffsetEffect", 
@@ -17,10 +19,14 @@ __all__ = [
     "PlasmaEffect",
     "GameOfLifeEffect",
     "RainEffect",
+    "MatrixEffect",
+    "DrawLines",
+    "_LINE",
     "Buffer",
     "BaseRenderer",
     "EffectRenderer",
     "CenterRenderer",
     "PanRenderer",
+    "FocusRenderer",
     "images"
 ]
